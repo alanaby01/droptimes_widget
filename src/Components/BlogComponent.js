b22f15f83b8182ed182e41c2464bc6a3ed27ccef
Alan P Aby <alanaby01@gmail.com>
2023-04-26 11:07:31 -0400

    Corrected the issue with Image URL

--- src/Components/BlogComponent.js
+++ src/Components/BlogComponent.js
@@ -2,10 +2,11 @@ import React from 'react';
 import '../css/blogcomponent.css';
 
 const BlogComponent = ({ title, image, link, date }) => {
+    const baseURL = "https://www.thedroptimes.com"
     return (
         <div class="blog-post-contents">
             <div class="blog-post-image">
-                <img src={image} alt="image" />
+                <img src={baseURL + image} alt="image" />
             </div>
             <div class="blog-post-info">
                 <h2 class="blog-post-title">{title}</h2>
