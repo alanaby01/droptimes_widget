3c5bc2ee36c27f3a1259e5969bb3ab32d3ecded2
Alan P Aby <alanaby01@gmail.com>
2023-04-29 22:36:05 -0400

    Updated server code

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const cors = require('cors');
 const app = express(); 
 const port = 8000;
 app.use(cors());
-
+//embed json here
 app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.sendFile(__dirname + '/index.html');
-    console.log(res);
+})
+
+app.get('/json-fetch', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
