978579c4a328d4e16b6e04dd203f8a4a911ef9d8
Alan P Aby <alanaby01@gmail.com>
2023-05-02 13:31:57 -0400

    JSON slicing from server complete

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+
+// var React = require('react');
+// var ReactDOMServer = require('react-dom/server');
+// var RootComponent = require('./src/App');
 
 const app = express(); 
 const port = 8000;
 app.use(cors());
 //embed json here
+// app.get('/', (req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.sendFile(__dirname + '/embed.html');
+// })
+
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.sendFile(__dirname + '/index.html');
-})
-
-app.get('/json-fetch', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/?num_rows=/', (req, res) => {
-    console.log(req.route.query.num_rows);
+    console.log(req.query.num_rows);
+    fs.readFile("./data.json", "utf8", (err, jsonString) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }
+        var itemstobeRendered = eval(jsonString).slice(0, req.query.num_rows);
+        var props = {num_rows: req.query.num_rows, items: itemstobeRendered};
+        console.log(props);
+    });
+    
+    
     res.send(200);
 });
 
